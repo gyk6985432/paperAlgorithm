73d1a832fd1b20668a15493c88c2d8bc72e508ca
gyk6985432 <gyk6985432@gmail.com>
2016-10-30 12:38:48 +0800

    updated

--- subProblems/BetterSolution.java
+++ subProblems/BetterSolution.java
@@ -11,8 +11,8 @@ public class BetterSolution {
     int maxLoad,minLoad;
     //1.对数组从大到小排序
     //由于机组数量一般不超过数十台，而且一般机组启停较少，当有新机组加入时，
-    // 其他机组已经排好了顺序，因此这种情况下用插入排序法比较合适
-    public void insert(){
+    // 其他机组已经排好了顺序，因此这种情况下用插入insert排序法比较合适
+    public void sort(){
         int i,j;
         Unit temp;
         for (i=1;i<units.length;i++) {
@@ -51,6 +51,7 @@ public class BetterSolution {
         return 0;
     }
     //3.列出台数范围内的所有可能，筛掉不符合最小负荷的组合
+    //http://www.geeksforgeeks.org/print-all-possible-combinations-of-r-elements-in-a-given-array-of-size-n/
     public int[][] getArrange(int minNum, int maxNum){
         int n1 = 1 << minNum;
         int n2 = 1 << maxNum;
@@ -58,7 +59,7 @@ public class BetterSolution {
         int count=0;
         for (int i=n1;i<n2;i++){
             int[] item = new int[units.length];
-            for (int j=0;j<maxNum;j++){
+            for (int j=0;j<units.length;j++){
                 int temp = i;
                 if ((temp & (1 << j)) != 0){
                     item[j] = 1;
@@ -67,5 +68,29 @@ public class BetterSolution {
             table[count++] = item;
         }
         return table;
+    }
+
+    public static void main(String[] args) {
+        Unit[] units = new Unit[5];
+        units[0] = new Unit(1000,500,3);
+        units[1] = new Unit(600,300,3);
+        units[2] = new Unit(500,250,3);
+        units[3] = new Unit(330,150,3);
+        units[4] = new Unit(300,0,3);
+        BetterSolution bs = new BetterSolution();
+        bs.units = units;
+        bs.maxLoad = 1400;
+        bs.minLoad = 1000;
+        bs.sort();
+        int minNum = bs.getMinNum();
+        int maxNum = bs.getMaxNum();
+        int[][] result = bs.getArrange(minNum,maxNum);
+        for (int i=0;i<result.length;i++){
+            for (int j=0;j<5;j++){
+                System.out.print(result[i][j]+" ");
+            }
+            System.out.println();
+        }
+
     }
 }
